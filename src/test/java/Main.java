106019f31b4526be5f2d6a89f5a12002104bc990
GonzaloBlondet <blondetg@gmail.com>
2022-08-20 14:03:42 -0400

    1

---
+++ src/test/java/Main.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriver a= null;
+    System.out.println("kjgabhjsd");
+
+    }
+
+}
