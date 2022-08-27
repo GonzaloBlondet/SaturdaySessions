e409a98a02318d18d8cacba38a4cda9874ad13bb
GonzaloBlondet <blondetg@gmail.com>
2022-08-27 13:39:59 -0400

    name

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,8 +2,8 @@ import org.openqa.selenium.WebDriver;
 
 public class Main {
     public static void main(String[] args) {
-        WebDriver a= null;
-    System.out.println("kjgabhjsd");
+       String name= "Gonzalo";
+
 
     }
 
