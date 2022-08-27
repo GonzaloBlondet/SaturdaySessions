57b35b08c2eda4eec422758424b270170555dc39
GonzaloBlondet <110270642+GonzaloBlondet@users.noreply.github.com>
2022-08-27 13:41:31 -0400

    Update Main.java

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -3,8 +3,8 @@ import org.openqa.selenium.WebDriver;
 public class Main {
     public static void main(String[] args) {
        String name= "Gonzalo";
-
-
+    
+    public void editing(){
     }
 
 }
